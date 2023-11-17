6010eafa2c8a6cc424b538083440121b856fd94d
Ryan <mathewsthomasryan@gmail.com>
2023-11-17 17:30:27 -0600

    func add params

--- calculator.js
+++ calculator.js
@@ -2,7 +2,9 @@ let num1 = 0;
 let num2 = 0;
 let operator = 0;
 
-function add() {
+
+
+function add(num1, num2, operator) {
 
 }
 
@@ -15,7 +17,7 @@ function multiply() {
 }
 
 function divide() {
-    
+
 }
 
 function operate() {
