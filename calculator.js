5cb2b43a61c0a2b8a1a6dcd6c4fc90e710217d34
Ryan <mathewsthomasryan@gmail.com>
2023-11-17 19:53:11 -0600

    const to let

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 let num1 = 0;
 let num2 = 0;
 let operator = 0;
-const screenDiv = document.querySelector('.screen');
+let screenDiv = document.querySelector('.screen');
 const buttonNine = document.getElementById('button9').value;
 
 
