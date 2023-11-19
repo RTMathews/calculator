3b48fda93e4ea518c96baa1ff1bc5e85d192f6b5
Ryan <mathewsthomasryan@gmail.com>
2023-11-18 18:20:03 -0600

    grid

--- calculator.js
+++ calculator.js
@@ -4,13 +4,12 @@ let operator = 0;
 let screenDiv = document.getElementById('screen');
 let buttonNine = document.getElementById('button9');
 
-function printNine()
+function printNumb(n)
 {
     let screenDiv = document.getElementById('screen');
-    screenDiv.textContent = '9';
 }
 
-buttonNine.addEventListener('click', printNine);
+buttonNine.addEventListener('click', printNumb);
 
 function add(num1, num2) {
     return num1 + num2;
